197d36d22b82228a372dd66d70afc702efb2637e
W <115349729+NRanggA@users.noreply.github.com>
2023-10-25 16:18:22 +0700

    Update calcu.js

--- calcu.js
+++ calcu.js
@@ -1,59 +1,57 @@
 function appendToResult(value) {
-  document.getElementById("input").value += value;
+  document.getElementById("result").value += value;
 }
 
 function clearResult() {
-  document.getElementById("input").value = "";
   document.getElementById("result").value = "";
 }
 
 function calculateResult() {
-  let result = eval(document.getElementById("input").value);
-  let result = eval(input);
+  let result = eval(document.getElementById("result").value);
   document.getElementById("result").value = result;
   window.alert("Hasilnya : " + result);
 }
 
 function calculateSqrt() {
-  let inputValue = document.getElementById("input").value;
+  let inputValue = document.getElementById("result").value;
   if (inputValue) {
     let sqrtResult = Math.sqrt(parseFloat(inputValue));
     document.getElementById("result").value = sqrtResult;
     window.alert("Akar dari " + inputValue + " = " + sqrtResult);
   } else {
-    document.getElementById("result").value = "";
+    window.alert("Please enter a valid number before taking the square root.");
   }
 }
 
 function calculateCos() {
-  let inputValue = document.getElementById("input").value;
+  let inputValue = document.getElementById("result").value;
   if (inputValue) {
     let cosResult = Math.cos((inputValue * Math.PI) / 180);
     document.getElementById("result").value = cosResult;
     window.alert("Cos dari " + inputValue + " = " + cosResult);
   } else {
-    document.getElementById("result").value = "";
+    window.alert("Masukkan angka sebelum menghitung Cos.");
   }
 }
 
 function calculateSin() {
-  let inputValue = document.getElementById("input").value;
+  let inputValue = document.getElementById("result").value;
   if (inputValue) {
     let sinResult = Math.sin((inputValue * Math.PI) / 180);
     document.getElementById("result").value = sinResult;
     window.alert("Sin dari " + inputValue + " = " + sinResult);
   } else {
-    document.getElementById("result").value = "";
+    window.alert("Masukkan angka sebelum menghitung Sin.");
   }
 }
 
 function calculateLog() {
-  let inputValue = document.getElementById("input").value;
+  let inputValue = document.getElementById("result").value;
   if (inputValue) {
     let logResult = Math.log(parseFloat(inputValue));
     document.getElementById("result").value = logResult;
     window.alert("Log dari " + inputValue + " = " + logResult);
   } else {
-    document.getElementById("result").value = "";
+    window.alert("Masukkan angka sebelum menghitung Log.");
   }
 }
